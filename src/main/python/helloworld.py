4ad74ca5bb4da5260fb218aba552cbd3c19d2d05
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-04-27 12:07:54 +0530

    Update helloworld.py

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -9,7 +9,7 @@ class TestStringMethods(unittest.TestCase):
 
 	# Returns True if the string contains 4 a.
 	def test_strings_a(self):
-		self.assertEqual( 'a'*4, 'aaaa')
+		self.assertEqual( 'a'*4, '1234')
 
 	# Returns True if the string is in upper case.
 	def test_upper(self):		
